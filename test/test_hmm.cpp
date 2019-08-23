8f966b68fb8fc404df49710f175395a5161f3e0a
huypn12 <huy.phung1292@gmail.com>
2019-08-23 20:59:20 +0200

    add test on observation stream

--- test/test_hmm.cpp
+++ test/test_hmm.cpp
@@ -6,38 +6,49 @@
 #include <memory>
 
 
-class fwd_bwd_fixture {
-private:
-  int n_state_;
-  int n_alphabet_;
-  Eigen::VectorXd init_;
-  Eigen::MatrixXd trans_;
-  Eigen::MatrixXd emit_;
-  void init_test_data() {
-    n_state_ = 3;
-    n_alphabet_ = 3;
-    init_ << 0.6, 0.4, 0;
-    trans_ << 0.69, 0.3, 0.01, 0.4, 0.59, 0.01, 0, 0, 1.0;
-    emit_ << 0.5, 0.4, 0.1, 0.1, 0.3, 0.6, 1.0, 0.0, 0.0;
-  }
 
+class fwd_bwd_fixture {
 protected:
-  std::shared_ptr<org::mcss::hmm> hmm_;
+  std::unique_ptr<org::mcss::hmm> hmm_;
 
 public:
-  fwd_bwd_fixture() {
-    hmm_ = std::make_shared<org::mcss::hmm>(n_state_, n_alphabet_, init_,
-                                            trans_, emit_);
-    init_test_data();
+  fwd_bwd_fixture(){
+    int n_state = 3;
+    int n_alphabet = 3;
+
+    auto init = Eigen::VectorXd(n_state);
+    auto trans = Eigen::MatrixXd(n_state, n_state);
+    auto emit = Eigen::MatrixXd(n_state, n_alphabet);
+
+    init << 0.6, 0.4, 0;
+    trans << 0.69, 0.3, 0.01, 0.4, 0.59, 0.01, 0, 0, 1.0;
+    emit << 0.5, 0.4, 0.1, 0.1, 0.3, 0.6, 1.0, 0.0, 0.0;
+
+   hmm_ = std::make_unique<org::mcss::hmm>(n_state, n_alphabet, init,
+                                            trans, emit);
   }
 };
 
-TEST_CASE_METHOD(fwd_bwd_fixture, "Test Forward Backward", "") {
+TEST_CASE_METHOD(fwd_bwd_fixture, "Test simulation", "[non-assert]") {
+  std::stringstream ss;
+  for (int i = 0; i < 100; i++) {
+    ss << hmm_->next_observation() << " ";
+  }
+  ss << std::endl;
+  INFO(ss.str());
+}
+
+TEST_CASE_METHOD(fwd_bwd_fixture, "Test info", "[non-assert]") {
+  INFO(hmm_->model_info());
+}
+
+TEST_CASE_METHOD(fwd_bwd_fixture, "Test Forward Backward", "[smooth]") {
   std::vector<int> observation({0, 1, 2});
-  Eigen::MatrixXd expected(2, 3);
+  Eigen::MatrixXd expected(3, 3);
   expected << 0.007518, 0.028120319999999997, 0.01,
-    0.01, 0.2109527048413057, 0.7890472951586943;
-  auto posterior_marginals = hmm_->smooth(observation);
+    0.01, 0.2109527048413057, 0.7890472951586943,
+    0,0,0;
+  auto posterior_marginals = hmm_->posterior(observation);
   REQUIRE(posterior_marginals.isApprox(expected));
 }
 
