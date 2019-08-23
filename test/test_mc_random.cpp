51a909504606de3d23f13578418ff411eb1b21c7
huypn12 <huy.phung1292@gmail.com>
2019-08-23 20:58:45 +0200

    add test on eigen l1 norm

--- test/test_mc_random.cpp
+++ test/test_mc_random.cpp
@@ -1,6 +1,17 @@
 //
 // Created by huypn12 on 6/11/19.
 //
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 
-#include <boost/test/unittest.hpp>
+#include <Eigen/Eigen>
 
+TEST_CASE("Eigen VectorXd normalization", "[Eigen]") {
+  Eigen::VectorXd v(2);
+  v << 1.0, 1.0;
+  Eigen::VectorXd expect(2);
+  expect << 0.5, 0.5;
+  v.lpNorm<1>();
+  std::cout << v;
+  REQUIRE(v.isApprox(expect));
+}
