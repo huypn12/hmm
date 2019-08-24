295e0838e4ce5002116e99c3de125e0f3038a62b
huypn12 <huy.phung1292@gmail.com>
2019-08-24 20:15:24 +0200

    move random to dtmc

--- include/dtmc.hpp
+++ include/dtmc.hpp
@@ -5,26 +5,26 @@
 #include "markov.hpp"
 
 namespace org::mcss {
-  class dtmc {
-  protected:
-    mc_random mc_random_;
+class dtmc {
+protected:
+  mc_random mc_random_;
 
-    int current_state_;
-    int previous_state_;
-    int states_size_;
-    Eigen::VectorXd initial_p_;
-    Eigen::MatrixXd transition_p_;
+  int current_state_ = -1;
+  int previous_state_ = -1;
+  int states_size_;
+  Eigen::VectorXd initial_p_;
+  Eigen::MatrixXd transition_p_;
 
-    int transition();
+  int transition();
 
-   public:
-    dtmc(int state_size, const Eigen::MatrixXd &p, const Eigen::VectorXd &p0);
+public:
+  dtmc(int state_size, const Eigen::VectorXd &p0, const Eigen::MatrixXd &p);
 
-    // markov trace stream
-    int next_state();
-    int get_current_state();
-    int get_previous_state();
-  };
+  // markov trace stream
+  int next_state();
+  int get_current_state();
+  int get_previous_state();
+};
 
 } // namespace org::mcss
 
--- src/dtmc.cpp
+++ src/dtmc.cpp
@@ -2,24 +2,20 @@
 
 using namespace org::mcss;
 
-dtmc::dtmc(int states_size,
-           const Eigen::MatrixXd& p, const Eigen::VectorXd& p0)
-  : states_size_(states_size),
-    transition_p_(p),
-    initial_p_(p0)
-{}
+dtmc::dtmc(int states_size, const Eigen::VectorXd &p0, const Eigen::MatrixXd &p)
+    : states_size_(states_size), transition_p_(p), initial_p_(p0) {}
 
-int dtmc::transition()
-{
+int dtmc::transition() {
+  if (current_state_ == -1) {
+    return mc_random_.choose_dirichlet(initial_p_);
+  }
   auto pij = transition_p_.row(current_state_);
   return mc_random_.choose_dirichlet(pij);
 }
 
-int dtmc::next_state()
-{
+int dtmc::next_state() {
   auto next_state = transition();
   previous_state_ = current_state_;
   current_state_ = next_state;
   return current_state_;
 }
-
