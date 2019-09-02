12e7365fec4378bb29204713e3fa957bc6823a9f
huypn12 <huy.phung1292@gmail.com>
2019-09-02 16:17:44 +0200

    add support for precompiled header, avoid slow compile due to eigen

---
+++ include/pchheader.hpp
@@ -0,0 +1,12 @@
+#ifndef __PCHHEADER_HPP__
+#define __PCHHEADER_HPP__
+
+#include <string>
+#include <vector>
+#include <random>
+#include <memory>
+#include <functional>
+
+#include <Eigen/Eigen>
+
+#endif
