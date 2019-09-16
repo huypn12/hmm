a9664f474bcb2f894d0b21868275515324ef6199
huypn12 <huy.phung1292@gmail.com>
2019-09-16 16:07:33 +0200

    remove submodules

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone --recurse-submodules https://github.com/huypn12/hmm.git
+git clone https://github.com/huypn12/hmm.git
 cd hmm
 cmake -H. -DCMAKE_BUILD_TYPE=Release -Bcmake-build-release -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
 cd cmake-build-release
