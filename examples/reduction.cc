aef7f910b2f149252c1b33abe151b1258ccc35a0
huypn12 <huy.phung1292@gmail.com>
2019-10-02 10:31:35 +0200

    expose tracefile to argv; add proper logging and tracefile reading

--- examples/reduction.cc
+++ examples/reduction.cc
@@ -7,35 +7,75 @@
 #include <memory>
 #include <unordered_map>
 
+#define PANIC -1
+#define OK 0
+
 using namespace org::mcss;
 
-static Logger logger("reduction.log");
+static Logger logger;
 
-void FitKStateModel(const int &state_count, const int &alphabet_count,
-                    const std::string &trace_file) {
-  auto test_model = std::make_unique<Hmm>(state_count, alphabet_count);
-  test_model->InitRandom();
-
-  logger.LogInfo("Test model before fitting \n" + test_model->Str());
-
-  auto trace = label_trace();
-  test_model->Fit(trace, 1000, 1e-4);
-
-  logger.LogInfo("Test model after fitting \n" + test_model->Str());
+int ReadTraceFile(const std::string &fpath, int &alphabet_count,
+                  std::string &trace_str) {
+  auto ifs = std::ifstream(fpath);
+  if (ifs.is_open()) {
+    auto line = std::string("");
+    int i = 0;
+    while (std::getline(ifs, line)) {
+      if (i == 0) {
+        alphabet_count = std::stoi(line);
+      } else if (i == 1) {
+        trace_str = line;
+      } else {
+        break;
+      }
+      i++;
+    }
+    return OK;
+  } else {
+    return PANIC;
+  }
 }
 
-void experiment() {
-  logger.LogInfo("Experiment: fitting with normal trace, jumps almost uniformly among states");
-  FitKStateModel(10, 3, "label_trace_1000_normal.txt");
-  FitKStateModel(5, 3, "label_trace_1000_normal.txt");
+int FitKStateModel(const int &state_count, const std::string &trace_file) {
+  int alphabet_count = 0;
+  auto trace_str = std::string("");
+  if (ReadTraceFile(trace_file, alphabet_count, trace_str) != OK) {
+    logger.LogError("Panic: File error " + trace_file);
+    return PANIC;
+  }
+  logger.LogInfo("+++ Hidden states: " + std::to_string(state_count));
+  logger.LogInfo("Using data file to train: " + trace_file);
+  logger.LogInfo("Label count: " + std::to_string(alphabet_count));
+  auto trace = label_trace(trace_str);
+  logger.LogInfo("Trace: " + trace.ToStr().substr(0, 10) + "... of size " +
+                 std::to_string(trace.size()));
 
-  logger.LogInfo("Experiment: fitting with abnormal trace, falls into spyder trap after few jumps");
-  FitKStateModel(10, 3, "label_trace_1000_abnormal.txt");
-  FitKStateModel(5, 3, "label_trace_1000_abnormal.txt");
+  auto test_model = std::make_unique<Hmm>(state_count, alphabet_count);
+  test_model->InitRandom();
+  logger.LogInfo("Test model before fitting \n" + test_model->Str());
+  test_model->Fit(trace, 1000, 1e-4);
+  logger.LogInfo("Test model after fitting \n" + test_model->Str());
+  logger.LogInfo("Loglikelihood: " +
+                 std::to_string(test_model->log_likelihood()));
+  logger.LogInfo("AIC: " + std::to_string(test_model->aic()));
+
+  return OK;
 }
 
 int main(int argc, char **argv) {
-  experiment();
+  if (argc < 3) {
+    std::cerr << "Usage: reduction <trace file> <state counts>[] \n";
+    return EXIT_FAILURE;
+  }
+
+  auto log_file = "reduction.log";
+  logger.SetLogFile(log_file);
+  logger.LogInfo("Start logging...");
+  auto trace_file = std::string(argv[1]);
+  for (int i = 2; i < argc; i++) {
+    auto state_count = std::stoi(argv[i]);
+    FitKStateModel(state_count, trace_file);
+  }
 
   return EXIT_SUCCESS;
 }
